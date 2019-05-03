3a7354be30885d82b34f963c3c5778450969d8a6
JX0Z <32142980+JX0Z@users.noreply.github.com>
2019-05-03 14:35:32 -0400

    Added Shapes

--- AsteroidCatcher/src/application/Main.java
+++ AsteroidCatcher/src/application/Main.java
@@ -1,20 +1,43 @@
 package application;
 	
+import java.awt.Canvas;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
-
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage game) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			
+			StackPane root = new StackPane();
+			game.setScene(new Scene(root,500,700));
+			game.show();
+			
+			Circle[] cirArray = new Circle[5];
+			for(int i=0;i<5;i++) {
+				cirArray[i]= new Circle(0,0,10);
+				root.getChildren().add(cirArray[i]);
+
+			}
+			
+			Rectangle platform = new Rectangle(50, 10);
+			root.getChildren().add(platform);
+			platform.setTranslateX(100);
+			platform.setTranslateX(0);
+
+			platform.addEventHandler(KeyEvent.ANY, event -> {
+				//System.out.println(event.getCode()==KeyCode.LEFTARROW);
+			});
+	
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
