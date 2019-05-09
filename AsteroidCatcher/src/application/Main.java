0ef42a80e09da57c9832c1ffb685607b207e577c
JX0Z <32142980+JX0Z@users.noreply.github.com>
2019-05-09 15:40:39 -0400

    Added Main menu and switching buttons

--- AsteroidCatcher/src/application/Main.java
+++ AsteroidCatcher/src/application/Main.java
@@ -1,7 +1,4 @@
 package application;
-	
-import java.awt.Canvas;
-import java.awt.TextArea;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -9,86 +6,120 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.*;
+import javafx.event.EventHandler;
 
 public class Main extends Application {
 	public double speed = 1.5;
-	public boolean platLeft = false; 
+	public boolean platLeft = false;
 	public boolean platRight = false;
 	public boolean gameOver = false;
 	public int powerSlowTime = 0;
 	public int powerLargeTime = 0;
 	public int score = 0;
+	static int xPositioning = 160, yPositioning = 50, ySpacing = 150;
+
 	@Override
-	public void start(Stage game) {
+	public void start(Stage primaryStage) {
 		try {
+			StackPane title = new StackPane();
+			Scene titleScene = new Scene(title, 500, 700);
+			primaryStage.setScene(titleScene);
+			StackPane game = new StackPane();
+			Scene gameScene = new Scene(game, 500, 700);
+			title.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+			Font buttonText = new Font(35);
+
+			// Background Image
+
+			// Creating the options for the title page
+			Button start = new Button("Play Now");
+			Button instructions = new Button("Rules");
+			Button end = new Button("Exit");
+
+			// Game Title
+			final Text gameTitle = new Text("ASTEROID CATCHER");
+			title.getChildren().add(gameTitle);
+			gameTitle.setFont(Font.font(STYLESHEET_CASPIAN, 30));
+			gameTitle.setFill(Color.CYAN);
+			gameTitle.setX(xPositioning - 60);
+			gameTitle.setY(yPositioning + 10);
+
+			// Adding the PLAY NOW button to the pane
+			title.getChildren().add(start);
+			start.setTranslateX(xPositioning - 35);
+			start.setTranslateY(yPositioning + ySpacing + 50);
+			start.setPrefSize(250, 100);
+			start.setFont(buttonText);
+
+			// Adding RULES button to the pane
+			title.getChildren().add(instructions);
+			instructions.setTranslateX(xPositioning - 35);
+			instructions.setTranslateY(yPositioning + ySpacing + 180);
+			instructions.setPrefSize(250, 100);
+			instructions.setFont(buttonText);
+
+			// Adding EXIT button to the pane
+			title.getChildren().add(end);
+			end.setTranslateX(xPositioning - 35);
+			end.setTranslateY(yPositioning + ySpacing + 310);
+			end.setPrefSize(250, 100);
+			end.setFont(buttonText);
+
+			primaryStage.setScene(titleScene);
+			primaryStage.show();
 			
-			StackPane root = new StackPane();
-			game.setScene(new Scene(root,500,700));
-			game.setTitle("Asteroid Catcher");
-			game.show();
-			//Init 5 Circles
+			// Init Power up
+			Circle powerSlow = new Circle(0, 0, 10, Color.GREEN);
+			powerSlow.setTranslateY((int) ((Math.random() * -1000) - 1000));
+			powerSlow.setTranslateX((int) (Math.random() * 460 + 20) - 250);
+			game.getChildren().add(powerSlow);
+
+			Circle powerLarge = new Circle(0, 0, 10, Color.YELLOW);
+			powerLarge.setTranslateY((int) ((Math.random() * -1000) - 1000));
+			powerLarge.setTranslateX((int) (Math.random() * 460 + 20) - 250);
+			game.getChildren().add(powerLarge);
+
+			// Init 5 Circles
 			Circle[] cirArray = new Circle[5];
-			for(int i=0;i<5;i++) {
-				cirArray[i]= new Circle(0,0,(int)(Math.random()*8+8));
-				cirArray[i].setTranslateX((int)(Math.random()*460+20)-250);
-				cirArray[i].setTranslateY(-350-i*150);
-				root.getChildren().add(cirArray[i]);
+			for (int i = 0; i < 5; i++) {
+				cirArray[i] = new Circle(0, 0, (int) (Math.random() * 8 + 8));
+				cirArray[i].setTranslateX((int) (Math.random() * 460 + 20) - 250);
+				cirArray[i].setTranslateY(-350 - i * 150);
+				game.getChildren().add(cirArray[i]);
 			}
-			//Init Power up
-			Circle powerSlow = new Circle(0,0,10,Color.GREEN);
-			powerSlow.setTranslateY((int)((Math.random()*-1000)-1000));
-			powerSlow.setTranslateX((int)(Math.random()*460+20)-250);
-			root.getChildren().add(powerSlow);
-			
-			Circle powerLarge = new Circle(0,0,10,Color.YELLOW);
-			powerLarge.setTranslateY((int)((Math.random()*-1000)-1000));
-			powerLarge.setTranslateX((int)(Math.random()*460+20)-250);
-			root.getChildren().add(powerLarge);
-			//Init scorebox
+			// Init scorebox
 			Text scoreBox = new Text();
 			scoreBox.setText("0");
 			scoreBox.setTranslateX(-230);
 			scoreBox.setTranslateY(-340);
-			root.getChildren().add(scoreBox);
-			//Init Player Platform
+			Circle scoreCircle = new Circle(50, Color.WHITE);
+			scoreCircle.setTranslateX(-250);
+			scoreCircle.setTranslateY(-350);
+			game.getChildren().add(scoreCircle);
+			game.getChildren().add(scoreBox);
+			// Init Player Platform
 			Rectangle platform = new Rectangle(100, 20, Color.BLUE);
-			platform.setTranslateX(0);
-			platform.setTranslateY(340);
-			root.getChildren().add(platform);
+				platform.setTranslateX(0);
+				platform.setTranslateY(340);
+			game.getChildren().add(platform);
 			
-			//Set booleans based on key presses
-			game.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
-				if(event.getCode()==KeyCode.LEFT) {
-					platLeft = true;
-				}
-				if(event.getCode()==KeyCode.RIGHT) {
-					platRight = true;
-				}
-			});
-			//Set booleans to false based on key releases
-			game.addEventHandler(KeyEvent.KEY_RELEASED, event -> {
-				if(event.getCode()==KeyCode.LEFT) {
-					platLeft = false;
-				}
-				if(event.getCode()==KeyCode.RIGHT) {
-					platRight = false;
-				}
-			});
-			
-	        new AnimationTimer() {
+			AnimationTimer gameLoop = new AnimationTimer() {
 	            @Override
 	            public void handle(long now) {
 	            	//Speeds up game by a tiny amount every frame
 	            	speed*=1.0005;
 	            	//Moves platform according to boolean as long as the platform isn't at the max
-	            	if (platLeft && !(platform.getTranslateX()<=-root.getWidth()/2+platform.getWidth()/2))platform.setTranslateX(platform.getTranslateX()-10);
-	            	if (platRight && !(platform.getTranslateX()>=root.getWidth()/2-platform.getWidth()/2))platform.setTranslateX(platform.getTranslateX()+10);
+	            	if (platLeft && !(platform.getTranslateX()<=-gameScene.getWidth()/2+platform.getWidth()/2))platform.setTranslateX(platform.getTranslateX()-10);
+	            	if (platRight && !(platform.getTranslateX()>=gameScene.getWidth()/2-platform.getWidth()/2))platform.setTranslateX(platform.getTranslateX()+10);
 	            	
 	            	//For loop for the asteroids
 	            	for(int i=0;i<5;i++) {
@@ -121,7 +152,7 @@ public class Main extends Application {
 	            			//Freezes all asteroids
 	            			speed=0;
 	            			//Expands the asteroid to fill the screen
-	            			if (cirArray[i].getRadius()<Math.sqrt(Math.pow(root.getWidth(),2)+Math.pow(root.getHeight(),2))) {
+	            			if (cirArray[i].getRadius()<Math.sqrt(Math.pow(gameScene.getWidth(),2)+Math.pow(gameScene.getHeight(),2))) {
 	            				cirArray[i].setRadius(cirArray[i].getRadius()+10);
 	            			}
 	            			else {
@@ -178,13 +209,62 @@ public class Main extends Application {
 	            		powerLarge.setTranslateX((int)(Math.random()*460+20)-250);
 	            	}
 	            }
-	        }.start();
-	
-		} catch(Exception e) {
+	        };
+	        
+			// Event Mouse Click
+			end.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					System.out.println("Application exited");
+					System.exit(0);
+				}
+			});
+			instructions.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					System.out.println("Game instructions displaying");
+					// New scene is displayed
+				}
+			});
+			start.setOnMouseClicked(new EventHandler<MouseEvent>() {
+				@Override
+				public void handle(MouseEvent event) {
+					System.out.println("Game began");
+					primaryStage.setScene(gameScene);
+					gameLoop.start();
+				}
+			});
+			
+			// Set booleans based on key presses
+			primaryStage.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+				if (event.getCode() == KeyCode.LEFT) {
+					platLeft = true;
+				}
+				if (event.getCode() == KeyCode.RIGHT) {
+					platRight = true;
+				}
+				if (event.getCode() == KeyCode.ESCAPE) {
+					primaryStage.setScene(titleScene);
+					System.out.println("Game stopped");
+					gameLoop.stop();
+				}
+			});
+			// Set booleans to false based on key releases
+			primaryStage.addEventHandler(KeyEvent.KEY_RELEASED, event -> {
+				if (event.getCode() == KeyCode.LEFT) {
+					platLeft = false;
+				}
+				if (event.getCode() == KeyCode.RIGHT) {
+					platRight = false;
+				}
+			});
+
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
